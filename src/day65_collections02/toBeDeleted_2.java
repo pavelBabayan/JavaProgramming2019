3fc074f99986fcd25f38bf869803097f431f5941
Pavel Babayan <babayan.pavel@gmail.com>
2019-07-14 18:45:46 -0400

    Second Update

---
+++ src/day65_collections02/toBeDeleted_2.java
@@ -0,0 +1,10 @@
+package day65_collections02;
+
+public class toBeDeleted_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+
+}
